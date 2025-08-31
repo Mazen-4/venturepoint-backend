bb80b5266bf20a2e3d31696fbe90b1575873415b
Mazen Ismail <mazen.ismail@msa.edu.eg>
2025-08-31 22:52:24 +0300

    Enable About, Services, and Contact routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,13 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-// import About from "./pages/About";
-// import Services from "./pages/Services";
+import About from "./pages/About";
+import Services from "./pages/Services";
 // import Projects from "./pages/Projects";
 // import Articles from "./pages/Articles";
 // import Events from "./pages/Events";
 // import Careers from "./pages/Careers";
-// import Contact from "./pages/Contact";
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -17,13 +17,13 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        {/* <Route path="/about" element={<About />} /> */}
-        {/* <Route path="/services" element={<Services />} /> */}
-        {/* <Route path="/projects" element={<Projects />} /> */}
+    <Route path="/about" element={<About />} />
+    <Route path="/services" element={<Services />} />
+    {/* <Route path="/projects" element={<Projects />} /> */}
   {/* <Route path="/articles" element={<Articles />} /> */}
-        {/* <Route path="/events" element={<Events />} /> */}
-        {/* <Route path="/careers" element={<Careers />} /> */}
-        {/* <Route path="/contact" element={<Contact />} /> */}
+    {/* <Route path="/events" element={<Events />} /> */}
+    {/* <Route path="/careers" element={<Careers />} /> */}
+    <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </Router>
